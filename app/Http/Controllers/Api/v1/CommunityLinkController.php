b21eb593bcd196f66f83f391f042b3a238909f8f
AlumG <AlumG@majada.es>
2024-11-18 18:22:47 +0100

    A22 pending

---
+++ app/Http/Controllers/Api/v1/CommunityLinkController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Models\CommunityLink;
+use Illuminate\Http\Request;
+use App\Queries\CommunityLinkQuery;
+
+class CommunityLinkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $query = new CommunityLinkQuery();
+
+        if (request()->exists('text')) {
+            $links = $query->search(request()->get('text'));
+        } else if (request()->exists('popular')) {
+            $links = $query->getMostPopular();
+        } else {
+            $links = $query->getAll();
+        }
+
+        $links = $links->latest('updated_at')->paginate(10);
+        return response()->json($links, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CommunityLink $communityLink)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CommunityLink $communityLink)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CommunityLink $communityLink)
+    {
+        //
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\v1\CommunityLinkController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::apiResource('v1/communitylinks', CommunityLinkController::class)->middleware('auth:sanctum');
